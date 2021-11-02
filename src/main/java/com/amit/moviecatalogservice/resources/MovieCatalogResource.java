4017d60943fb214fb66d6e96acffcc9b1d80818a
007amits <amitshukla5502@gmail.com>
2021-11-03 01:34:42 +0530

    Added rest template

--- src/main/java/com/amit/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/amit/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,11 +1,15 @@
 package com.amit.moviecatalogservice.resources;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import com.amit.moviecatalogservice.models.CatalogItem;
+import com.amit.moviecatalogservice.models.Movie;
+import com.amit.moviecatalogservice.models.Rating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -14,9 +18,19 @@ public class MovieCatalogResource {
   @RequestMapping("/{userId}")
   public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
     
-    return Collections.singletonList(
-        new CatalogItem("Transformers", "test", 4)
+    RestTemplate restTemplate = new RestTemplate();
+    
+    
+    List<Rating> ratings = Arrays.asList(
+        new Rating("1234",4),
+        new Rating("5678",5)
     );
+    
+    return ratings.stream().map(rating -> {
+      Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+      return new CatalogItem(movie.getName(), "Description", rating.getRating());
+    }).collect(Collectors.toList());
+    
   }
 
 }
