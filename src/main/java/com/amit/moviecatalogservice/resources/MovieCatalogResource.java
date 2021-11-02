37a11e48f146ed59a48dac21442828ae38415d59
007amits <amitshukla5502@gmail.com>
2021-11-03 01:59:12 +0530

    using web client instead of rest template

--- src/main/java/com/amit/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/amit/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import com.amit.moviecatalogservice.models.CatalogItem;
 import com.amit.moviecatalogservice.models.Movie;
 import com.amit.moviecatalogservice.models.Rating;
@@ -19,6 +20,9 @@ public class MovieCatalogResource {
   @Autowired
   private RestTemplate restTemplate;
   
+  @Autowired
+  private WebClient.Builder builder;
+  
   @RequestMapping("/{userId}")
   public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
     
@@ -28,7 +32,13 @@ public class MovieCatalogResource {
     );
     
     return ratings.stream().map(rating -> {
-      Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+      //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+      Movie movie = builder.build()
+      .get()
+      .uri("http://localhost:8082/movies/"+rating.getMovieId())
+      .retrieve()
+      .bodyToMono(Movie.class)
+      .block();
       return new CatalogItem(movie.getName(), "Description", rating.getRating());
     }).collect(Collectors.toList());
     
