67f4922c70f5b725b09e7f074b7aa39536648ae2
007amits <amitshukla5502@gmail.com>
2021-11-03 02:24:34 +0530

    Making call to ratings data service

--- src/main/java/com/amit/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/amit/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
@@ -14,13 +13,8 @@ public class MovieCatalogServiceApplication {
     return new RestTemplate();
   }
   
-  @Bean
-  public WebClient.Builder getWebClientBuilder(){
-    return WebClient.builder();
+  public static void main(String[] args) {
+    SpringApplication.run(MovieCatalogServiceApplication.class, args);
   }
-
-	public static void main(String[] args) {
-		SpringApplication.run(MovieCatalogServiceApplication.class, args);
-	}
 
 }
--- src/main/java/com/amit/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/amit/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,6 +1,5 @@
 package com.amit.moviecatalogservice.resources;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 import com.amit.moviecatalogservice.models.CatalogItem;
 import com.amit.moviecatalogservice.models.Movie;
-import com.amit.moviecatalogservice.models.Rating;
+import com.amit.moviecatalogservice.models.UserRating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -20,25 +18,13 @@ public class MovieCatalogResource {
   @Autowired
   private RestTemplate restTemplate;
   
-  @Autowired
-  private WebClient.Builder builder;
-  
   @RequestMapping("/{userId}")
   public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
     
-    List<Rating> ratings = Arrays.asList(
-        new Rating("1234",4),
-        new Rating("5678",5)
-    );
+    UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
     
-    return ratings.stream().map(rating -> {
-      //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
-      Movie movie = builder.build()
-      .get()
-      .uri("http://localhost:8082/movies/"+rating.getMovieId())
-      .retrieve()
-      .bodyToMono(Movie.class)
-      .block();
+    return ratings.getUserRating().stream().map(rating -> {
+      Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
       return new CatalogItem(movie.getName(), "Description", rating.getRating());
     }).collect(Collectors.toList());
     
