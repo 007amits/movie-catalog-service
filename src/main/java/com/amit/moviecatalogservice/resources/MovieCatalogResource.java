58415e0b72885014bd710b12e484040cd0e6601f
007amits <amitshukla5502@gmail.com>
2021-11-03 01:42:59 +0530

    Added bean to create instance

--- src/main/java/com/amit/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/amit/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.amit.moviecatalogservice.resources;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,11 @@ import com.amit.moviecatalogservice.models.Rating;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
   
+  @Autowired
+  private RestTemplate restTemplate;
+  
   @RequestMapping("/{userId}")
   public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-    
-    RestTemplate restTemplate = new RestTemplate();
-    
     
     List<Rating> ratings = Arrays.asList(
         new Rating("1234",4),
